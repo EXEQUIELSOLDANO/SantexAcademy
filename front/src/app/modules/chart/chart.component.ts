aacd1b18bd8ee2b4703cb7da2f12c9033f86ec6b
Javier Leonel Sosa <leo.vm.cba@gmail.com>
2023-10-06 22:02:40 -0300

    agregado: carga dinamica de datos segun el chart

--- front/src/app/modules/chart/chart.component.ts
+++ front/src/app/modules/chart/chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, OnInit, Input, ElementRef, ViewChild, AfterViewInit, OnChanges, SimpleChanges } from '@angular/core';
 import { Chart, ChartOptions, ChartTypeRegistry } from 'chart.js/auto';
 
 @Component({
@@ -6,10 +6,12 @@ import { Chart, ChartOptions, ChartTypeRegistry } from 'chart.js/auto';
   templateUrl: './chart.component.html',
   styleUrls: ['./chart.component.css']
 })
-export class ChartComponent implements AfterViewInit {
+export class ChartComponent implements AfterViewInit, OnChanges {
   @ViewChild('chartCanvas') chartCanvas!: ElementRef;
   @Input() chartType: keyof ChartTypeRegistry = 'bar';
   @Input() chartData: any;
+
+  private chart: Chart | undefined; // Almacenar una referencia al gráfico
 
   constructor() { }
 
@@ -17,58 +19,70 @@ export class ChartComponent implements AfterViewInit {
     this.createChart();
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    // Verificar si chartData cambió y crear el gráfico nuevamente
+    if (changes['chartData'] && this.chartData) {
+      this.createChart();
+    }
+  }
+
   createChart(): void {
-    if (this.chartData) {
-      const dataLabels = Object.keys(this.chartData.estadisticas);
-      
-      const labels = dataLabels.map(key => this.chartData.estadisticas[key].opcion);
-
-      const data = dataLabels.map(key => this.chartData.estadisticas[key].porcentaje);
-  
-
-      const chartOptions: ChartOptions = {
-        responsive: true,
-        maintainAspectRatio: true,
-        plugins: {
-          title: {
+    // Verificar que chartCanvas esté definido
+    if (this.chartCanvas && this.chartCanvas.nativeElement) {
+      if (this.chartData && this.chartData.estadisticas) { // Verificar que chartData y estadisticas estén definidos
+        const dataLabels = Object.keys(this.chartData.estadisticas);
+        const labels = dataLabels.map(key => this.chartData.estadisticas[key].opcion);
+        const data = dataLabels.map(key => this.chartData.estadisticas[key].porcentaje);
+    
+        const chartOptions: ChartOptions = {
+          responsive: true,
+          maintainAspectRatio: true,
+          plugins: {
+            title: {
               display: false
+            }
           }
+        };
+    
+        const chartData = {
+          labels: labels,
+          datasets: [{
+            label: 'Porcentaje',
+            data: data,
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.4)',
+              'rgba(255, 159, 64, 0.4)',
+              'rgba(75, 192, 192, 0.4)',
+              'rgba(153, 102, 255, 0.4)',
+              'rgba(54, 162, 235, 0.4)',
+              'rgba(201, 203, 207, 0.4)',
+              'rgba(255, 205, 86, 0.4)'
+            ],
+            borderColor: [
+              'rgb(255, 99, 132)',
+              'rgb(255, 159, 64)',
+              'rgb(255, 205, 86)',
+              'rgb(75, 192, 192)',
+              'rgb(54, 162, 235)',
+              'rgb(153, 102, 255)',
+              'rgb(201, 203, 207)'
+            ],
+            borderWidth: 1
+          }]
+        };
+    
+        const ctx = this.chartCanvas.nativeElement;
+    
+        if (this.chart) {
+          this.chart.destroy();
         }
-      };
-
-      const chartData = {
-        labels: labels,
-        datasets: [{
-          label: 'Porcentaje',
-          data: data,
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.4)',
-            'rgba(255, 159, 64, 0.4)',
-            'rgba(75, 192, 192, 0.4)',
-            'rgba(153, 102, 255, 0.4)',
-            'rgba(54, 162, 235, 0.4)',
-            'rgba(201, 203, 207, 0.4)',
-            'rgba(255, 205, 86, 0.4)'
-          ],
-          borderColor: [
-            'rgb(255, 99, 132)',
-            'rgb(255, 159, 64)',
-            'rgb(255, 205, 86)',
-            'rgb(75, 192, 192)',
-            'rgb(54, 162, 235)',
-            'rgb(153, 102, 255)',
-            'rgb(201, 203, 207)'
-          ],
-          borderWidth: 1
-        }]
-      };
-
-      const ctx = this.chartCanvas.nativeElement.getContext('2d');
-      new Chart(ctx, {
-        type: this.chartType,
-        data: chartData,
-        options: chartOptions
-      });
+    
+        this.chart = new Chart(ctx, {
+          type: this.chartType,
+          data: chartData,
+          options: chartOptions
+        });
+      }
     }
   }
 }
--- front/src/app/modules/stats/stats.component.ts
+++ front/src/app/modules/stats/stats.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { forkJoin } from 'rxjs';
+import { StatsService } from 'src/app/core/services/stats.service';
 
 @Component({
   selector: 'app-stats',
   templateUrl: './stats.component.html',
   styleUrls: ['./stats.component.css']
 })
-export class StatsComponent {
+export class StatsComponent implements OnInit {
+  procedencia: any;
+  razones: any;
+  tipoHospedaje: any;
+  solicitaronInfo: any;
+  tipoInfoSolicitada: any;
+  
+  
+  constructor(private statsService: StatsService){ }
+
   statsData = {
     "estadisticas": {
         "4": {
@@ -29,5 +40,30 @@ export class StatsComponent {
             "porcentaje": 26
         }
     }
+  }
+
+  ngOnInit(): void {
+    this.stats()
+  }
+
+  getStatsData(id: number) {
+    // Llama al servicio para obtener los datos de estadísticas por ID
+    return this.statsService.getStatsByQuestion(id)
+  }
+
+  stats(){
+    forkJoin([
+      this.getStatsData(3),
+      this.getStatsData(6),
+      this.getStatsData(8),
+      this.getStatsData(11),
+      this.getStatsData(13),
+    ]).subscribe(([procedenciaData, razonesData, tipoHospedajeData, solicitaronInfoData, tipoInfoSolicitadaData]) => {
+      this.procedencia = procedenciaData;
+      this.razones = razonesData;
+      this.tipoHospedaje = tipoHospedajeData;
+      this.solicitaronInfo = solicitaronInfoData;
+      this.tipoInfoSolicitada = tipoInfoSolicitadaData;
+    });
   }
 }
