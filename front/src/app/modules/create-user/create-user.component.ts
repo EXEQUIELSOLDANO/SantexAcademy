4202e1a434633e7d756cca92516e4491edcc8ccb
vani <you@example.com>
2023-10-08 01:19:15 -0300

    modificado:La función que  realiza una solicitud para obtener información  de un usuario, ya sea un encuestador  o un admin, dependiendo del valor del parámetro roll para editar

--- front/src/app/modules/create-user/create-user.component.ts
+++ front/src/app/modules/create-user/create-user.component.ts
@@ -21,17 +21,64 @@ export class CreateUserComponent implements OnInit {
   tipoUsuario = '';
   error = false;
   updateAdminsService: any;
-  idUsuario = -1;
+  idUsuario = 0;
   roll: any;
 
+  constructor(private router: Router, private http: HttpClient, private adminsService: AdminsService, private route: ActivatedRoute, private pollstersService: PollstersService) { }
 
+  ngOnInit(): void {
+    const data = this.route.snapshot.data
+    this.text = data["text"];
+    this.idUsuario = parseInt(this.route.snapshot.params['id']) ;
+    this.roll = this.route.snapshot.params['roll'];
+
+    if (this.idUsuario && this.roll) {
+      
+      if (this.roll === "encuestador") {
+        this.pollstersService.getOnePollsterById(this.idUsuario).subscribe({
+          next: (data) => {
+            this.nombre = data.firstname;
+            this.apellido = data.lastname;
+            this.dni = data.dni.toString();
+            this.telefono = data.phone.toString();
+            this.direccion = data.adress;
+            this.email = data.email;
+            this.roll = data.roll;
+            this.tipoUsuario = this.roll
+            console.log(data)
+            //TODO agregar los campos
+          }
+        })
+      }
+
+      if (this.roll === "admin") {
+        this.adminsService.getAdminById(this.idUsuario).subscribe({
+          next: (data) => {
+            this.nombre = data.firstname;
+            this.apellido = data.lastname;
+            this.dni = data.dni.toString();
+            this.telefono = data.phone.toString();
+            this.direccion = data.adress;
+            this.email = data.email;
+            this.roll = data.roll;
+            this.tipoUsuario = this.roll
+            console.log(data)
+            //TODO agregar los campos
+          }
+        })
+      }
+    }
+
+  }
 
   submitUser() {
+    
     //Validacion
     if (this.nombre === '' || this.apellido === '' || this.dni === '' || this.telefono === '' || this.direccion === '' || this.email === '' || this.tipoUsuario === '') {
       this.showError();
       return
     }
+
     this.usuario = {
       firstname: this.nombre,
       lastname: this.apellido,
@@ -47,12 +94,15 @@ export class CreateUserComponent implements OnInit {
     this.redirectTo(this.usuario.roll)
 
   }
+
   updateUser() {
+    
     //Validacion
     if (this.nombre === '' || this.apellido === '' || this.dni === '' || this.telefono === '' || this.direccion === '' || this.email === '' || this.tipoUsuario === '') {
       this.showError();
       return
     }
+
     this.usuario = {
       firstname: this.nombre,
       lastname: this.apellido,
@@ -64,56 +114,12 @@ export class CreateUserComponent implements OnInit {
       poll_id: 0,
       roll: this.tipoUsuario,
     };
-    this.redirectToUpdate(this.usuario.roll, 1)
-  }
-  constructor(private router: Router, private http: HttpClient, private adminsService: AdminsService, private route: ActivatedRoute, private pollstersService: PollstersService) { }
-
-  ngOnInit(): void {
-    const data = this.route.snapshot.data
-    this.text = data["text"];
-    let id = this.route.snapshot.paramMap.get("id");
-    let roll = this.route.snapshot.paramMap.get("roll");
-
-
-    if (id && roll) {
-      this.idUsuario = parseInt(id, 10);
-      if (roll === "encuestador") {
-        this.pollstersService.getOnePollsterById(this.idUsuario).subscribe({
-          next: (data) => {
-            this.nombre = data.firstname;
-            this.apellido = data.lastname;
-            this.dni = data.dni.toString();
-            this.telefono = data.phone.toString();
-            this.direccion = data.adress;
-            this.email = data.email;
-            this.roll = data.id;
-            console.log(data)
-            //TODO agregar los campos
-          }
-        })
-      }
-      if (roll === "admin") {
-        this.adminsService.getAdminById(this.idUsuario).subscribe({
-          next: (data) => {
-            this.nombre = data.firstname;
-            this.apellido = data.lastname;
-            this.dni = data.dni.toString();
-            this.telefono = data.phone.toString();
-            this.direccion = data.adress;
-            this.email = data.email;
-            this.roll = data.id;
-            console.log(data)
-            //TODO agregar los campos
-          }
-        })
-      }
-    }
-
-  }
+    this.redirectToUpdate(this.usuario.roll, this.idUsuario)
+  }  
 
   redirectTo(tipo: string): void {
     if (tipo === 'admin') {
-      //this.adminsService .submitAdmin(this.usuario);
+
       console.log('administrador creado', this.usuario);
       this.adminsService.submitAdmin(this.usuario).subscribe({
         next: () => {
@@ -124,13 +130,11 @@ export class CreateUserComponent implements OnInit {
           console.log('Ocurrio un error en la DB')
         }
       })
-    } else /*if (this.usuario.roll === 'encuestador')*/ {
-      //this.pollstersService.submitPollsters(this.usuario);
+    } else {
 
       delete this.usuario.poll_id;
 
-
-      console.log('encuestador creado', this.usuario)
+      console.log('encuestador creado',this.usuario )
       this.pollstersService.submitPollster(this.usuario).subscribe({
         next: () => {
           console.log('Encuestador guardado en la DB');
@@ -142,28 +146,26 @@ export class CreateUserComponent implements OnInit {
       })
     }
   }
+
   redirectToUpdate(tipo: string, id: number): void {
     if (tipo === 'admin') {
-      //this.adminsService .updateAdmin(this.usuario);
-      console.log('administrador editado', this.usuario);
+
       this.adminsService.updateAdmin(this.usuario, id).subscribe({
         next: () => {
-          console.log('Admin guardado en la DB')
+          console.log('Admin editado en la DB')
           this.redirectToUpDateSuccess();
         },
         error: () => {
           console.log('Ocurrio un error en la DB')
         }
       })
-    } else /*if (this.usuario.roll === 'encuestador')*/ {
-      //this.pollstersService.updatePollsters(this.usuario);
+    } else {
+
       delete this.usuario.poll_id;
 
-
-      console.log('encuestador editado', this.usuario)
       this.pollstersService.updatePollster(this.usuario, id).subscribe({
         next: () => {
-          console.log('Encuestador guardado en la DB');
+          console.log('Encuestador editado en la DB');
           this.redirectToUpDateSuccess();
         },
         error: () => {
